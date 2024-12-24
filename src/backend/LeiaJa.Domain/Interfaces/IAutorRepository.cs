8a1f58868703b4b232554cc6bf8db667e51d6dc7
Adyllson <domingosady11son@gmail.com>
2024-12-24 14:18:11 +0100

    Instalando os pacotes do Nuget

--- src/backend/LeiaJa.Domain/Interfaces/IAutorRepository.cs
+++ src/backend/LeiaJa.Domain/Interfaces/IAutorRepository.cs
@@ -1,5 +1,9 @@
 namespace LeiaJa.Domain.Interfaces;
 public interface IAutorRepository
 {
-    Task<ResponseModel<List<AutorEntity>>> GetAllAsync();
+    Task<ResponseModel<List<AutorEntity>>> GetAllAutoresAsync();
+    Task<ResponseModel<List<AutorEntity>>> CreateAutorAsync(AutorEntity autor);
+    Task<ResponseModel<AutorEntity>> UpdateAutorAsync(AutorEntity autor);
+    Task<ResponseModel<AutorEntity?>> DeleteAutorAsync(int autorId);
+    Task<ResponseModel<AutorEntity>?> GetAutorByIdAsync(int autorId);
 }
