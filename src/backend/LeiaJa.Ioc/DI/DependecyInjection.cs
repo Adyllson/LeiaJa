47bb0c732307059664e4798902ecbd6db19546fb
Adyllson <domingosady11son@gmail.com>
2024-12-25 13:25:12 +0100

    Criando as Interfaces dos Repositorios e Serviços

--- src/backend/LeiaJa.Ioc/DI/DependecyInjection.cs
+++ src/backend/LeiaJa.Ioc/DI/DependecyInjection.cs
@@ -20,11 +20,31 @@ public static class DependecyInjection
         #region REPOSITORY
             services.AddScoped<IAutorRepository, AutorRepository>();
             services.AddScoped<ISistemaRepository, SistemaRepository>();
+            services.AddScoped<IProvinciaRepository, ProvinciaRepository>();
+            services.AddScoped<IMunicipioRepository, MunicipioRepository>();
+            services.AddScoped<ITipoUsuarioRepository, TipoUsuarioRepository>();
+            services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+            services.AddScoped<ITipoTelefoneRepository, TipoTelefoneRepository>();
+            services.AddScoped<ITelefoneRepository, TelefoneRepository>();
+            services.AddScoped<ICategoriaRepository, CategoriaRepository>();
+            services.AddScoped<ILivroRepository, LivroRepository>();
+            services.AddScoped<IEmprestimoRepository, EmprestimoRepository>();
+            services.AddScoped<IGeneroRepository, GeneroRepository>();
         #endregion
 
         #region SERVICE
             services.AddScoped<IAutorService, AutorService>();
             services.AddScoped<ISistemaService, SistemaService>();
+            services.AddScoped<IProvinciaService, ProvinciaService>();
+            services.AddScoped<IMunicipioService, MunicipioService>();
+            services.AddScoped<ITipoUsuarioService, TipoUsuarioService>();
+            services.AddScoped<IUsuarioService, UsuarioService>();
+            services.AddScoped<ITipoTelefoneService, TipoTelefoneService>();
+            services.AddScoped<ITelefoneService, TelefoneService>();
+            services.AddScoped<ICategoriaService, CategoriaService>();
+            services.AddScoped<ILivroService, LivroService>();
+            services.AddScoped<IEmprestimoService, EmprestimoService>();
+            services.AddScoped<IGeneroService, GeneroService>();
         #endregion
 
         #region AUTOMAPPER
